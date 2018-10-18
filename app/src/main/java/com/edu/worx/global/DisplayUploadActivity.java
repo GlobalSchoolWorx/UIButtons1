1767f9d272249ab7af66efc81dbe7f778d15bc71
Global School Worx <globalschoolworx@gmail.com>
2018-10-18 13:31:37 +0530

    first commit

---
+++ app/src/main/java/com/edu/worx/global/DisplayUploadActivity.java
@@ -0,0 +1,13 @@
+package com.edu.worx.global;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_upload);
+    }
+}
